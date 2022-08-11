41c54eff61ce3023d81542e9cfa6f2f5fcaee8a2
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2022-08-11 12:32:01 -0300

    adds other WS hw

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,15 @@
 var fs  = require("fs")
 var http  = require("http")
 
+http.createServer((req, res)=>{
+        fs.readFile(`${__dirname}/images/${req.url}.jpg`, (err, data)=>{
+            if(err){
+                res.writeHead(404, { 'Content-Type':'text/plain' });
+                res.end('Hubo un error')
+            } else {
+                res.writeHead(200, { 'Content-Type':'image/jpg' })
+                res.end(data);
+            }
+        }); 
+}).listen(1337, '127.0.0.1');
 // Escribí acá tu servidor
